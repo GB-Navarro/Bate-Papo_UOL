af8a515a9f4aabf22a8778d7e55f56055e9752f8
GB-Navarro <devnavarro06@gmail.com>
2022-02-09 11:55:07 -0300

    Added a connection to an external server

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+let promisse = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+
+function getMesssages(){
+    let promisse = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promisse.then
+}
+
+setTimeout(getMessages, 3000);
